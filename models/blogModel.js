d9896878f82eb8e2ef99098248e812ea2d2b1074
hoseacodes <dominique11h@yahoo.com>
2021-05-18 12:47:37 -0500

    first commit

---
+++ models/blogModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema(
+	{
+		category: Object,
+		name: Object,
+		date: Object,
+		img: String,
+		title: Object,
+		type: Object,
+		info: Object,
+		subHeading: Object,
+		tags: Object,
+		link: Object,
+	},
+	{
+		timestamps: true,
+	}
+);
+
+module.exports = mongoose.model("Blog", blogSchema);
